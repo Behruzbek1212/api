bad6b213bbfbf57e78599f52383cebfed3f1655a
Ibrohim <58683257+ibrohim777775@users.noreply.github.com>
2023-06-17 18:35:22 +0500

    Update admin.php

--- routes/admin.php
+++ routes/admin.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Admin\GuidesController;
 use App\Http\Controllers\Admin\JobsController;
 use App\Http\Controllers\Admin\ResumeController;
 use App\Http\Controllers\Admin\StatisticAdminController;
-use App\Http\Controllers\ResumeController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/jobs')->name('jobs.')->group(function () {
