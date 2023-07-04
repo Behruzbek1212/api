92dd57cfacf28cacda727b12f29a01cee982ceb9
bexruz01 <bexruzbaxromov.01@gmail.com>
2023-07-04 14:02:35 +0500

    change

--- app/Http/Middleware/IsAdminMiddleware.php
+++ app/Http/Middleware/IsAdminMiddleware.php
@@ -10,8 +10,8 @@ class IsAdminMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
-        // if ((!_auth()->check()) && (!@$request->user('sanctum')->role == 'admin'))
-        if ((_auth()->check()) && (!in_array($request->user('sanctum')->role, ['admin', 'hr', 'recruiter']))) {
+        if ((!_auth()->check()) && (!@$request->user('sanctum')->role == 'admin')) {
+            // if ((_auth()->check()) && (!in_array($request->user('sanctum')->role, ['admin', 'hr', 'recruiter']))) {
             return response()->json([
                 'status' => false,
                 'message' => 'This is not possible  to enter!'
