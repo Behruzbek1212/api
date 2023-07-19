e0aed19ac38946f3a679efea9ede76cdefb92717
bexruz01 <bexruzbaxromov.01@gmail.com>
2023-07-19 16:44:56 +0500

    change

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,16 @@ Route::get('/', function () {
 });
 
 Route::any('/handle/{paysys}', function ($paysys) {
-    return response()->json((new Goodoneuz\PayUz\PayUz)->driver($paysys)->handle());
+    (new Goodoneuz\PayUz\PayUz)->driver($paysys)->handle();
+});
+
+
+//redirect to payment system or payment form
+Route::any('/pay/{paysys}/{key}/{amount}', function ($paysys, $key, $amount) {
+    $model = Goodoneuz\PayUz\Services\PaymentService::convertKeyToModel($key);
+    $url = request('redirect_url', '/'); // redirect url after payment completed
+    $pay_uz = new Goodoneuz\PayUz\PayUz;
+    $pay_uz
+        ->driver($paysys)
+        ->redirect($model, $amount, 860, $url);
 });
