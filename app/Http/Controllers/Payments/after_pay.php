8b04648075da98b4592b4c94f4eaefbee697bde6
bexruz01 <bexruzbaxromov.01@gmail.com>
2023-07-31 15:04:17 +0500

    change transaction

--- app/Http/Controllers/Payments/after_pay.php
+++ app/Http/Controllers/Payments/after_pay.php
@@ -1,1 +1,6 @@
 <?php
+$total_amount =  \App\Models\Transaction::where('transactionable_id', $transaction->transactionable_id)->where('state', 2)->sum('amount') ?? 0;
+        \App\Models\User::where('id', $transaction->transactionable_id)
+            ->update([
+                'balance' => $total_amount
+            ]);
