6df63d072b92637d8730ff14da3afa5d9c941822
Ibrohim <58683257+ibrohim777775@users.noreply.github.com>
2023-08-16 18:52:34 +0500

    Update isCustomerMiddleware.php

--- app/Http/Middleware/isCustomerMiddleware.php
+++ app/Http/Middleware/isCustomerMiddleware.php
@@ -12,24 +12,31 @@ class isCustomerMiddleware
     {
         // dd($request->user()->customer()->active);
         // $request->header('customer_id');
-        if (in_array(_user()->role, ['admin'])) {
-            return $next($request);
-        }
-        $roles_count = Customer::query()->where('id', $request->header('customer_id'))
-            ->where('active', true)->count();
+        // if (in_array(_user()->role, ['admin'])) {
+        //     return $next($request);
+        // }
+        // $roles_count = Customer::query()->where('id', $request->header('customer_id'))
+        //     ->where('active', true)->count();
 
-        if ($roles_count > 0) {
-            if (!in_array(_user()->role, ['admin', 'customer'])) {
-                return response()->json([
-                    'status' => false,
-                    'message' => 'This is not possible for candidates!'
-                ]);
-            }
-            return $next($request);
+        // if ($roles_count > 0) {
+        //     if (!in_array(_user()->role, ['admin', 'customer'])) {
+        //         return response()->json([
+        //             'status' => false,
+        //             'message' => 'This is not possible for candidates!'
+        //         ]);
+        //     }
+        //     return $next($request);
+        // }
+        // return response()->json([
+        //     'status' => false,
+        //     'message' => 'This is not possible for candidates! or not active customer'
+        // ]);
+         if (!in_array($request->user()->role, ['admin', 'customer'])) {
+            return response()->json([
+                'status' => false,
+                'message' => 'This is not possible for candidates!'
+            ]);
         }
-        return response()->json([
-            'status' => false,
-            'message' => 'This is not possible for candidates! or not active customer'
-        ]);
-    }
+         return $next($request);
+}
 }
