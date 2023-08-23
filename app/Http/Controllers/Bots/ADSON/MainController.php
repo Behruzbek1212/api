473860e7cf638415f0956e66fc2553a8426f21fd
ibrohim777775 <ibrohim777775@gmail.com>
2023-08-23 17:10:47 +0500

    update MainController on adson bot

--- app/Http/Controllers/Bots/ADSON/MainController.php
+++ app/Http/Controllers/Bots/ADSON/MainController.php
@@ -4,56 +4,85 @@ namespace App\Http\Controllers\Bots\ADSON;
 
 use App\Http\Controllers\Controller;
 use App\Models\Bot\Adson;
-use App\Models\Bot\AdsonCrater;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    public function addLinks(Request $request)
+    public function store(Request $request)
+    {
+        $credentials = $request->validate([
+            'uuid' => ['uuid', 'required'],
+            'identification' => ['string', 'required'],
+            'info' => ['array', 'required']
+        ]);
+
+        $data = Adson::query()->firstOrCreate([
+            'uuid' => $credentials['uuid']
+        ], $credentials)->get(['uuid', 'identification', 'info']);
+
+        return response()->json([
+            'status' => true,
+            'data' => $data
+        ]);
+    }
+
+    public function check(Request $request)
     {
         $credentials = $request->validate([
             'identification' => ['string', 'required'],
-            'url' => ['string', 'required'],
-            'image' => ['string', 'required']
+            'telegram_id' => ['numeric', 'required']
         ]);
 
-        AdsonCrater::query()->updateOrCreate([
-            'identification' => $credentials['identification']
-        ], $credentials);
+        $model = Adson::query()->where('uuid', '=', $credentials['identification']);
+        $model->update(['telegram_id' => $credentials['telegram_id']]);
+
+        $data = $model->first(['uuid', 'identification', 'info']);
 
         return response()->json([
             'status' => true,
+            'data' => $data
         ]);
     }
 
-    public function getUsers()
-    {
-        $list = Adson::query()->distinct('telegram_id')->get(['telegram_id']);
-
-        return response()->json([
-            'status' => true,
-            'data' => $list
-        ]);
-    }
-
-    public function getUser(Request $request)
+    public function getUrl(Request $request)
     {
         $credentials = $request->validate([
-            'uuid' => ['string', 'required']
+            'identification' => ['string', 'required']
         ]);
 
-        $user = Adson::query()->where($credentials)
-            ->first(['telegram_id']);
+        $data = Adson::query()->where('uuid', '=', $credentials['identification'])
+            ->first(['info']);
 
-        if ( $user == null ) {
+        $crate = Adson::query()->where('uuid', '=', $credentials['identification'])
+            ->first()->link;
+
+        return response()->json([
+            'status' => true,
+            'data' => $data['info'],
+            'url' => $crate['url'],
+            'image' => $crate['image']
+        ]);
+    }
+
+    public function getInfo(Request $request)
+    {
+        $credentials = $request->validate([
+            'telegram_id' => ['numeric', 'required']
+        ]);
+
+        $data = Adson::query()->where('telegram_id', '=', $credentials['telegram_id'])
+            ->first(['info']);
+
+        if ( $data == null ) {
             return response()->json([
-                'status' => false
+                'status' => true,
+                'data' => null
             ]);
         }
 
         return response()->json([
             'status' => true,
-            'data' => $user['telegram_id']
+            'data' => $data['info']
         ]);
     }
 }
