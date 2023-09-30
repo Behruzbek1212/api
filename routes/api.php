ea20d19af6d812e2b2e8e2811ec9e1c39de16150
asadbek <abduvaliyevasad23@gmail.com>
2023-09-30 17:00:08 +0500

    block number

--- routes/api.php
+++ routes/api.php
@@ -72,8 +72,8 @@ Route::prefix('/v1')->group(function () {
         $data = ["refresh"=> 0, "items"=> [
             ["number"=> "001", "name"=> "Name 1", "firstname"=> "", "lastname"=> "", "phone"=> "947980058", "mobile"=> "947980058", "email"=> "", "address"=> "", "city"=> "", "state"=> "", "zip"=> "", "comment"=> "", "presence"=> 0, "starred"=> 0, "info"=> ""],
             ["number"=> "002", "name"=> "Name 2", "firstname"=> "", "lastname"=> "", "phone"=> "+998947980058", "mobile"=> "+998947980058", "email"=> "", "address"=> "", "city"=> "", "state"=> "", "zip"=> "", "comment"=> "", "presence"=> 0, "starred"=> 0, "info"=> ""],
-            ["number"=> "002", "name"=> "Name 2", "firstname"=> "", "lastname"=> "", "phone"=> "+998993960990", "mobile"=> "+998993960990", "email"=> "", "address"=> "", "city"=> "", "state"=> "", "zip"=> "", "comment"=> "", "presence"=> 0, "starred"=> 0, "info"=> ""],
-            ["number"=> "002", "name"=> "Name 2", "firstname"=> "", "lastname"=> "", "phone"=> "993960990", "mobile"=> "993960990", "email"=> "", "address"=> "", "city"=> "", "state"=> "", "zip"=> "", "comment"=> "", "presence"=> 0, "starred"=> 0, "info"=> ""],
+            ["number"=> "003", "name"=> "Name 2", "firstname"=> "", "lastname"=> "", "phone"=> "+998993960990", "mobile"=> "+998993960990", "email"=> "", "address"=> "", "city"=> "", "state"=> "", "zip"=> "", "comment"=> "", "presence"=> 0, "starred"=> 0, "info"=> ""],
+            ["number"=> "004", "name"=> "Name 2", "firstname"=> "", "lastname"=> "", "phone"=> "993960990", "mobile"=> "993960990", "email"=> "", "address"=> "", "city"=> "", "state"=> "", "zip"=> "", "comment"=> "", "presence"=> 0, "starred"=> 0, "info"=> ""],
         ]];
 
         return  response()->json($data);
