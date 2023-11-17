f2304ec433bd93d4d3659634065a6772e36cdacc
asadbek <abduvaliyevasad23@gmail.com>
2023-11-17 14:36:45 +0500

    bug test result

--- app/Repository/TestResultRepository.php
+++ app/Repository/TestResultRepository.php
@@ -129,14 +129,19 @@ class TestResultRepository
         $customer_id = $request->customer_id ?? null;
 
         if($customer_id !== null){
-            $result = $this->user->candidate->testResult()->where('deleted_at', null)->where('customer_id', $customer_id)->firstOrFail();
+            if($this->user->candidate !== null){
+                $result = $this->user->candidate->testResult()->where('deleted_at', null)->where('customer_id', $customer_id)->firstOrFail();
+
+                return $result;
+            }
+            return [];
+        }
+        if($this->user->candidate !== null){
+            $result = $this->user->candidate->testResult()->where('deleted_at', null)->where('customer_id', null)->firstOrFail();
 
             return $result;
         }
-        
-        $result = $this->user->candidate->testResult()->where('deleted_at', null)->where('customer_id', null)->firstOrFail();
-
-        return $result;
+        return [];
     }
 
     public function show($request)
