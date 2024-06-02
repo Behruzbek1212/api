119b702a20c3eebe61f082c948d3a890b2d06a59
Asadbek <103238177+Abduvaliyev2003@users.noreply.github.com>
2024-06-02 23:03:29 +0500

    Update JobServices.php

--- app/Services/JobServices.php
+++ app/Services/JobServices.php
@@ -109,7 +109,7 @@ class JobServices
 
         list($firstLine, $remainingText) = $this->getFirstAndRemainingLines($trimmedString);
 
-        $jpgImage->text($postNumber, 1020, 430, function ($font) use ($fontPath) {
+        $jpgImage->text($postNumber, 1010, 415, function ($font) use ($fontPath) {
             $font->file($fontPath);
             $font->size(70);
             $font->color('#057AF5');
@@ -137,7 +137,7 @@ class JobServices
             });
         }
 
-        $jpgImage->text($text2, 400, 1350, function ($font) use ($gilroyLight) {
+        $jpgImage->text($text2, 415, 1380, function ($font) use ($gilroyLight) {
             $font->file($gilroyLight);
             $font->size(47);
             $font->color('#474747');
@@ -145,7 +145,7 @@ class JobServices
             $font->valign('middle');
         });
 
-        $jpgImage->text($text3, 1420, 1350, function ($font) use ($gilroyLight) {
+        $jpgImage->text($text3, 1390, 1380, function ($font) use ($gilroyLight) {
             $font->file($gilroyLight);
             $font->size(47);
             $font->color('#474747');
